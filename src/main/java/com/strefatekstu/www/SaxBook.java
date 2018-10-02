e899389219eb1f2e25acbb1c1d9cc5eaaddbc1cd
kamilnaja <kamilnaja@gmail.com>
2018-10-02 18:53:50 +0200

    added saxbook

--- src/main/java/com/strefatekstu/www/SaxBook.java
+++ src/main/java/com/strefatekstu/www/SaxBook.java
@@ -6,8 +6,7 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.io.InputStream;
-import java.net.URL;
+import java.io.File;
 
 public class SaxBook {
     public static void main(String[] args) throws Exception {
@@ -16,26 +15,28 @@ public class SaxBook {
             url = "http://www.w3c.org";
             System.out.println("Using " + url);
         } else url = args[0];
+//        File file = new File("employee.xml");
+//todo - odczytać dane z pliku
 
         DefaultHandler handler = new DefaultHandler() {
             @Override
             public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-                if (localName.equalsIgnoreCase("a") && attributes != null) {
+
+                if (localName.equalsIgnoreCase("name")) {
                     for (int i = 0; i < attributes.getLength(); i++) {
                         String aname = attributes.getLocalName(i);
-                        if (aname.equals("href")) {
-                            System.out.println(attributes.getValue(i));
-                        }
+                        System.out.println(aname);
                     }
                 }
             }
         };
+
         SAXParserFactory factory = SAXParserFactory.newInstance(); // zwraca instancję klasy SAXParseFactory
         factory.setNamespaceAware(true);  // spr
-        factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
         SAXParser saxParser = factory.newSAXParser(); // zwraca instancję klasy SAXParser
-        InputStream in = new URL(url).openStream();
-        saxParser.parse(in, handler);
+        File fileToParse = new File("Employee.xml");
+
+        saxParser.parse(fileToParse, handler);
         // todo - jak otworzyć plik na dysku
     }
 }
